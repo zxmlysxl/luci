0723d9f7754cfdf65ae602f6f03821e43601e37d
zxmlysxl <zxmlysxl@gmail.com>
2022-02-20 13:21:31 +0800

    更改部分插件设置

--- applications/luci-app-openvpn/luasrc/controller/openvpn.lua
+++ applications/luci-app-openvpn/luasrc/controller/openvpn.lua
@@ -5,7 +5,7 @@
 module("luci.controller.openvpn", package.seeall)
 
 function index()
-	entry( {"admin", "services", "openvpn"}, cbi("openvpn"), _("OpenVPN") )
-	entry( {"admin", "services", "openvpn", "basic"},    cbi("openvpn-basic"),    nil ).leaf = true
-	entry( {"admin", "services", "openvpn", "advanced"}, cbi("openvpn-advanced"), nil ).leaf = true
+	entry( {"admin", "vpn", "openvpn"}, cbi("openvpn"), _("OpenVPN") )
+	entry( {"admin", "vpn", "openvpn", "basic"},    cbi("openvpn-basic"),    nil ).leaf = true
+	entry( {"admin", "vpn", "openvpn", "advanced"}, cbi("openvpn-advanced"), nil ).leaf = true
 end
